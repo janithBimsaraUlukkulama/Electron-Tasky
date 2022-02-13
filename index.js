eab6524e4f1145fe0409ad4254d833f924ab889e
janithBimsaraUlukkulama <jbimsaraulukkulama@outlook.com>
2022-02-13 15:42:39 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const electron = require('electron');
+const path = require('path');
+const {Tray} = require("electron");
+const {app, BrowserWindow} = electron;
+
+let mainWindow, tray;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        // webPreferences: {nodeIntegration: true, contextIsolation: false},
+        height: 500,
+        width: 300,
+        frame: false,
+        resizable: false,
+        show: false
+    });
+    mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+
+    const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
+    const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
+    tray = new Tray(iconPath);
+    tray.on('click', (event, bounds) => {
+        // Click event bounds
+        const {x, y} = bounds;
+
+        //window height and width
+        const {height, width} = mainWindow.getBounds();
+
+        if (mainWindow.isVisible()) {
+            mainWindow.hide();
+
+        } else {
+            const yPosition = process.platform === 'darwin' ? y : y - height;
+            mainWindow.setBounds({
+                x: x - width / 2,
+                y:yPosition,
+                height: height,
+                width: width
+            });
+            mainWindow.show();
+
+        }
+    });
+});
